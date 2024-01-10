e6c7078436a3180f1d34435391b2e99ae9738500
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2024-01-10 19:46:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ function convert1(){
      var fromcurrency = document.getElementById("from").value
      var tocurrency = document.getElementById("to").value
      var oldamount = document.getElementById("amount").value
-     var url = 'https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency?have='+fromcurrency+'&want='+tocurrency+'&amount='+oldamount;
-     const options = {
+
+	 //'https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from='+fromcurrency+'&to='+tocurrency+'&amount='+oldamount
+
+     //var url = 'https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency?have='+fromcurrency+'&want='+tocurrency+'&amount='+oldamount;
+     var url =  'https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from='+fromcurrency+'&to='+tocurrency+'&amount='+oldamount
+	 const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'd98a809651msha857f5479c53644p1e168djsn833e37ebc1df',
-      'X-RapidAPI-Host': 'currency-converter-by-api-ninjas.p.rapidapi.com'
+		'X-RapidAPI-Key': 'ad0c9c7bd0msh01ec56be200f01dp14e47ajsn69c552a045a5',
+		'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
     }
   }; 
 
   fetch(url,options)
   .then(response => response.json())
+  //.then(response => console.log(Object.values(response)[2].rate))
   //.then(response => console.log(Object.values(response)))
-  .then(response=>document.getElementById("output").innerHTML=(Object.values(response)[0])+' '+tocurrency)
+  .then(response=>document.getElementById("output").innerHTML=(Object.values(response)[2].rate)+' '+tocurrency)
   .then(response => document.getElementById("input").innerHTML= oldamount+' '+fromcurrency+' '+'=')
   }
