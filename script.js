522fb79271da3c06e6259dcc48165867347f387a
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2024-04-13 12:07:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function convert1(){
   //.then(response => console.log(Object.values(response)[2].rate))
   //.then(response => console.log(Object.values(response)))
 	  //the get get modified and upended
+	  //takes value from the object in server
   .then(response=>document.getElementById("output").innerHTML=(oldamount*(Object.values(response)[2].rate)+' '+tocurrency))
   .then(response => document.getElementById("input").innerHTML= oldamount+' '+fromcurrency+' '+'=')
   }
